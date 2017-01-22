fe69480721fa92e8a3e2a53442d1e1918cb92bc4
Kevin <keyeh@calpoly.edu>
2017-01-22 15:08:52 -0800

    AirPump upload current state when set over MQTT

--- src/GrowProgram/private/AirPump.cpp
+++ src/GrowProgram/private/AirPump.cpp
@@ -8,12 +8,15 @@ airPumpNode("air_pump", "relay")
 void AirPump::setup() {
 	Homie.getLogger() << F("AirPump::setup()") << endl;
 	pinMode(AIR_PUMP_PIN, OUTPUT);
-  digitalWrite(AIR_PUMP_PIN, HIGH);
+
+	digitalWrite(AIR_PUMP_PIN, HIGH);
   airPumpOn = true;
-  airPumpNode.advertise("on").settable([&](const HomieRange& range, const String& value) {
+
+	airPumpNode.advertise("on").settable([&](const HomieRange& range, const String& value) {
 		if (value != "true" && value != "false") return false;
 		airPumpOn = (value == "true");
 		digitalWrite(AIR_PUMP_PIN, airPumpOn ? HIGH : LOW);
+		uploadCurrentState();
 		return true;
 	});
 }
