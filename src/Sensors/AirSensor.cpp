a5d931815e18cd81ec02d4e4367994bffd14d5ff
Kevin <keyeh@calpoly.edu>
2017-02-28 01:01:27 -0800

    humidity

--- src/Sensors/AirSensor.cpp
+++ src/Sensors/AirSensor.cpp
@@ -4,11 +4,12 @@ AirSensor::AirSensor():
 air_sensor(AIR_SENSOR_PIN, AIR_SENSOR_TYPE)
 {
   air_temp = 0;
+  air_humidity = 0;
   air_sensor_last_read = 0;
 }
 
 void AirSensor::loop() {
-  if (millis() - air_sensor_last_read >= 5000) {
+  if (millis() - air_sensor_last_read >= 2000) {
     air_sensor_last_read = millis();
     readSensor();
   }
@@ -23,13 +24,14 @@ float AirSensor::getAirHumidity() {
 }
 
 void AirSensor::readSensor() {
-  float new_air_temp_f = air_sensor.readTemperature(true);
+  float new_air_temp = air_sensor.readTemperature(true);
+  float new_air_humidity = air_sensor.readHumidity();
 
-  if (isnan(new_air_temp_f)) {
+  if (isnan(new_air_temp)) {
     Serial.println("Failed to read air_sensor!");
   } else {
-    air_temp = new_air_temp_f;
-
-    Serial << "Temperature: " << air_temp << " °F" << endl;
+    air_temp = new_air_temp;
+    air_humidity = new_air_humidity;
+    Serial << "Temperature: " << air_temp << " °F, Humidity: " << air_humidity << "% RH" << endl;
   }
 }
