03a179f9d4af109ad73b75f6931a7221a8383fb7
Kevin <keyeh@calpoly.edu>
2017-04-26 13:58:26 -0700

    air sensor request

--- src/Sensors/AirSensor.cpp
+++ src/Sensors/AirSensor.cpp
@@ -6,6 +6,9 @@ AirSensor::AirSensor()
   air_sensor_data.humidity = 0.0;
   air_sensor_data.last_read = 0;
   air_sensor_data.last_read_success = false;
+
+  last_request = 0;
+  last_request_success = false;
 }
 
 void AirSensor::byte_to_struct_2(byte *a) {
@@ -17,18 +20,21 @@ void AirSensor::byte_to_struct_2(byte *a) {
 }
 
 void AirSensor::loop() {
-  if (millis() - air_sensor_data.last_read >= 2000) {
+  if (millis() - last_request >= 2000) {
+    last_request = millis();
+
     const int struct_size = sizeof(air_sensor_data);
     byte buf [struct_size];
-
     if (Wire.requestFrom(HWC_I2C_ID, struct_size)) {
       // if request succeeded
+      last_request_success = true;
       for (byte i = 0; i < struct_size; i++)
         buf [i] = Wire.read ();
 
       byte_to_struct_2(buf);
     }
     else {
+      last_request_success = false;
       Serial.println(F("I2C failed"));
     }
   }
--- src/Sensors/AirSensor.hpp
+++ src/Sensors/AirSensor.hpp
@@ -5,6 +5,8 @@
 class AirSensor {
   private:
     AirSensorData air_sensor_data;
+    unsigned long last_request;
+    bool last_request_success;
     void byte_to_struct_2(byte *a);
   public:
     AirSensor();
